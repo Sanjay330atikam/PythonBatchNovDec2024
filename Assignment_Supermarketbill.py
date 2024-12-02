8babed06fe4f98005831c1b3820091c0f66a9383
Sanjay330atikam <atikamsanjay330@gmail.com>
2024-12-02 03:42:17 +0000

    Assignment_Supermarketbill.py

---
+++ Assignment_Supermarketbill.py
@@ -0,0 +1,8 @@
+print(""" Dear {Customer},
+           Your account ending {account_number}  was balance 
+           of {Balance_amount}.
+      """.format(
+               Customer="Sanjay",
+               account_number ="12345",
+               Balance_amount ="50,000"
+           ))
