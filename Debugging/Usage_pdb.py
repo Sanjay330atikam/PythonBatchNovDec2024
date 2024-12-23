d609c34606bda771271aab4e46aa7f5c6e385573
Sanjay330atikam <atikamsanjay330@gmail.com>
2024-12-23 04:15:09 +0000

    Debugging/Usage_pdb.py

---
+++ Debugging/Usage_pdb.py
@@ -0,0 +1,12 @@
+val1  = int(input("enter the Val1 = "))
+val2  = int(input("enter the Val2 = "))
+
+# print(val1,val2)
+import pdb
+pdb.set_trace()
+
+# import pdb; pdb.set_trace()
+
+# # Multiplication
+result = val1 * val2 # val1 + val2 
+print(f" Multiplication :{result}")
