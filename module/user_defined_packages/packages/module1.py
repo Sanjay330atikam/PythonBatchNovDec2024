af380fd12b89edf8dacc36659e878427c649f421
Sanjay330atikam <atikamsanjay330@gmail.com>
2025-01-14 03:35:13 +0000

    module/user_defined_packages/

---
+++ module/user_defined_packages/packages/module1.py
@@ -0,0 +1,16 @@
+import sys
+
+def Hello_World():
+    print("Hello -----!!!!!!")
+
+
+if __name__ == "__main__": 
+       Hello_World()
+else:
+     print(
+          f"""
+          {__name__ =}
+          {__package__ =}
+        """
+
+       )
