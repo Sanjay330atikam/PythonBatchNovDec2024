edc526b8eab18f2c457f149fcaec4abf7e3799d0
Sanjay330atikam <atikamsanjay330@gmail.com>
2025-01-28 04:27:19 +0000

    02_instantination.py

--- OOPS/02_instantination.py
+++ OOPS/02_instantination.py
@@ -19,4 +19,23 @@ print(f"{dir(c1) =}")
 print()
 
 print(f"callable.number    :{callable(c1.number)}")#False beacuse number is varibale it doesnot have function call
-print(f"c1.number           :{c1.number}") 
+print(f"c1.number           :{c1.number}")# accesig values directly
+
+# you get value of any attribute by using getattr
+print(f"getattr(c1,number)     :{getattr(c1,"number")}")
+
+#checks hasattr will check whether whether particular object any kind of support 
+# Hasattr will manly check whether particular attribute is present or not
+print(f"hasattr(c1,number)  :{hasattr(c1,"number")}")
+print(f"'number'in dir(c1)    :{'number'in dir(c1)}")
+
+#Classe are mutuable objects
+c1.number = 1000
+print(f"getattr(c1,'number)     :{getattr(c1,'number')}")
+print(f"{id(c1) =}")
+
+setattr(c1,"number3",3333)
+print(f"getattr(c1,number3)      :{getattr(c1,'number3')}")
+
+c1.number4 =4444
+print(f"getattr(c1,number4)      :{getattr(c1,'number4')}")
