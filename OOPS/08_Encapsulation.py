ad86a76b3e6cd4f5fda83103808255de73a7fd5f
Sanjay330atikam <atikamsanjay330@gmail.com>
2025-02-01 03:37:22 +0000

    OOPS/08_Encapsulation.py

--- OOPS/08_Encapsulation.py
+++ OOPS/08_Encapsulation.py
@@ -37,3 +37,34 @@ class Car():
         print(f"self.__b ", {self.__b})
         print(f"self.instance_method",{self.instance_method})
         print(f"Car.a:",{Car.a})
+
+if __name__ == "__main__":
+    print("\n-----Class Variable------")
+    print(vars(Car))#vars can be accesed at class level #means vars will acess only clas varaibles
+    {
+        "a " : "public class variable",
+       "_a" : "protected class variable",
+      "_Car__a ": "private class variable"
+    }
+print("Car.a",Car.a)# acessing public varaible
+print("Car._a",Car._a)# acesssing protected variable
+print("_Car.__a",Car._Car__a)# accesing prvate class varibale is bit different
+# - first defined with Class name(_Car) and then __a(private class varaibale)
+
+print("\n--Instance Level")
+#Instantation
+c=Car()
+print(vars(c))
+print()
+
+print(f"\nc.b    :{c.b}")
+print(f"\nc._b   :{c._b}")
+print(f"\nc._Car__b    :{c._Car__b}")
+print()
+
+c.instance_method()#public
+c._instance_method()#protected
+c._Car__instance_method()#private varibale can be accesed by prepend Class name in protected manner and append with private varibale
+print()
+
+c.new_method()
